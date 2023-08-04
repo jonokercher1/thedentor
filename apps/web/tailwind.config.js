da0a4fae1111f2c05bc22578e99381445a31d679
Jono <jonokercher@gmail.com>
2023-08-04 08:39:21 +0100

    configure tailwind

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -7,11 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        accent: {
+          primary: {
+            light: '#83D4F1',
+            DEFAULT: '#60D1FA',
+            dark: '#38B7E5',
+          },
+          secondary: {
+            light: '#ACB8FF',
+            DEFAULT: '#8494F8',
+            dark: '#6C7FF5',
+          },
+        },
+        state: {
+          success: '#6FAD67'
+        },
+        neutral: {
+          100: '#FFFFFF',
+          300: '#CBCFD5',
+          500: '#41546E',
+          700: '#162D4C',
+          900: '#091728'
+        }
+      }
     },
   },
   plugins: [],
